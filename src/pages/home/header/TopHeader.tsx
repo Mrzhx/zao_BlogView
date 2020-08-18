3198d64572a126840033806e236fa5d4477b90cb
mrzhx <zhx_silentheart@163.com>
2020-08-18 21:36:01 +0800

    feat: 导航

--- src/pages/home/header/TopHeader.tsx
+++ src/pages/home/header/TopHeader.tsx
@@ -2,39 +2,67 @@ import React from 'react'
 import { UnorderedListOutlined } from '@ant-design/icons';
 import './TopHeader.css';
 
-// const navItem = [
-//   {
-//     name: "首页",
-//     url: "/",
-//   },
-//   {
-//     name: "文章",
-//     url: "/article",
-//   },
-//   {
-//     name: "关于我们",
-//     url: "/detail",
-//   },
-//   {
-//     name: "联系我们",
-//     url: "/phone",
-//   },
-//   {
-//     name: "加入我们",
-//     url: "/join",
-//   }
-// ]
+const navItem = [
+  {
+    id: 0,
+    name: "首页",
+    url: "/",
+  },
+  {
+    id: 1,
+    name: "文章",
+    url: "/article",
+  },
+  {
+    id: 2,
+    name: "关于我们",
+    url: "/detail",
+  },
+  {
+    id: 3,
+    name: "联系我们",
+    url: "/phone",
+  },
+  {
+    id: 4,
+    name: "加入我们",
+    url: "/join",
+  }
+]
 
 interface Props {
   history: any
 };
 
+interface NavCon {
+  nav_status?: Boolean,
+  child?: {
+    name: string,
+    url: string
+  }
+}
+
+const NavItem: React.FC<NavCon> = (value) => {
+  console.log(value)
+  return(
+    <li>lll</li>
+  )
+}
+
+
+const Navlist: React.FC = () => {
+  const itemArr = navItem.map((item) => {
+    <NavItem key={item.id} child={item}></NavItem>
+  })
+  return <ul>{itemArr}</ul>
+}
 
 const UserHeader: React.FC = () => {
   return (
     <div className="layout-header">
       <div className="nav-con">
         <h4>前端早早聊大会</h4>
+        <Navlist></Navlist>
         <UnorderedListOutlined />
       </div>
     </div>
@@ -45,7 +73,7 @@ export const TopHeader: React.FC<Props> = (props) => {
   // const ChangeUrl = (event: any) => {
   //   props.history.push(event.key);
   // };
-
+  console.log(props)
   return (
     <UserHeader></UserHeader>
   )
